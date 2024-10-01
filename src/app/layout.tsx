da00746b5109c064323d0a9c845e177a2bc056fb
Chirag Bhatia <chiragbhatia.here2005@gmail.com>
2024-10-01 14:31:03 +0530

    blog project done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,11 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
-import Head from "next/head";  // Import Head for the script injection
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Blog Creator",
-  description: "Blog Creator, write blogs effortlessly",
+  description: "Blog Creator , write blogs effortlesly",
 };
 
 interface RootLayoutProps {
@@ -18,21 +16,7 @@ interface RootLayoutProps {
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <Head>
-        {/* Google Analytics Script */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2Q5MXLD22K"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-2Q5MXLD22K');
-            `,
-          }}
-        />
-      </Head>
+    <html lang="en" >
       <AuthProvider>
         <body className={inter.className}>
           {children}
